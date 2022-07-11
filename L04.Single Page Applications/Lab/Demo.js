9c822fe007ad18efe58239f333a530119324e4d3
Angel Mladenov <48069264+velk20@users.noreply.github.com>
2022-07-11 22:19:51 +0300

    5

--- L04.Single Page Applications/Lab/Demo.js
+++ L04.Single Page Applications/Lab/Demo.js
@@ -1,5 +1,7 @@
+import { sum as renamedSum } from './DemoModule.js';
 import { sum, divideTwoNumbers } from './DemoModule.js';
 import * as api from './DemoModule.js';
 
 console.log(sum(5, 2));
+console.log(renamedSum(3, 3));
 console.log(api.sum(2, 2));

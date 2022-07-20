467e69a7ab232894b4e45e153a133e639cd34b12
Angel <48069264+velk20@users.noreply.github.com>
2022-07-20 13:42:24 +0300

    1

--- L05.Architecture and Testing/Lab/Demo.js
+++ L05.Architecture and Testing/Lab/Demo.js
@@ -1,1 +1,12 @@
-function demo() {}
+const { chromium } = require('playwright-chromium');
+const { expect } = require('chai');
+
+let browser, page;
+(async () => {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://google.com/');
+  await page.screenshot({ path: `example.png` });
+  await browser.close();
+})();
--- L06.Client Side Rendering/Lab/Demo.js
+++ L06.Client Side Rendering/Lab/Demo.js
@@ -1,1 +1,4 @@
-function demo() {}
+import { html, render } from 'lit-html';
+
+const greetings = (name) => html`<h2>${name}</h2>`;
+render(greetings, window);
